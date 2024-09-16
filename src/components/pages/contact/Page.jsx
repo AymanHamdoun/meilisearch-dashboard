65344c9b76c629f7198e2cf305117afca3d57522
Ayman <ayman.s.hamdoun@gmail.com>
2024-09-16 22:50:44 +0400

    add contact form

--- src/components/pages/contact/Page.jsx
+++ src/components/pages/contact/Page.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import ContactForm from "./Form.jsx";
 
+/**
+ * Contact page component that renders the ContactForm.
+ * @component
+ * @returns {JSX.Element} The rendered contact page.
+ */
 const Page = () => {
     return <div id="pageHome" className={"p-5 flex flex-col items-center"}>
-        <h3 className={"font-bold text-4xl text-gray-700"}>Contact</h3>
+        <h3 className={"mb-5 font-bold text-4xl text-gray-700"}>Contact</h3>
+        <div>
+            <ContactForm/>
+        </div>
     </div>
 }
 
