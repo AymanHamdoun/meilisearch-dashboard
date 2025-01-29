289dc498b079ccaa7a887ec44edec10a3124fb7d
Ayman <ayman.s.hamdoun@gmail.com>
2025-01-29 12:18:32 +0400

    add default value

--- src/components/pages/instance/index/settings/RankingInfo.tsx
+++ src/components/pages/instance/index/settings/RankingInfo.tsx
@@ -11,9 +11,13 @@ const RankingInfo = () => {
         <DocHeader
             title={"Ranking Rules"}
             badge={"rankingRules"}
-            description={"The complete list of ranking rules used for relevancy tuning. Default value: ['*']"}
+            description={"The complete list of ranking rules used for relevancy tuning. Default value:"}
             link={"https://www.meilisearch.com/docs/learn/relevancy/ranking_rules"}
         />
+        <div className={"mb-4"}>
+            <code className={"bg-gray-100 text-sm p-1 rounded"}>[ "words", "typo", "proximity", "attribute", "sort",
+                "exactness" ]</code>
+        </div>
         <DynamicTextBoxes
             buttonText="+ Add a Ranking Attribute"
             initialTextboxValues={settings.rankingRules}
